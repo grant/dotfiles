2abf5a0ad047a31a158538d08cb8e5a48d3b4517
Grant Timmerman <granttimmerman@gmail.com>
2019-04-02 22:39:37 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This command:
+# curl https://raw.githubusercontent.com/grant/dotfiles/master/setup.sh | sh
+
+curl https://raw.githubusercontent.com/grant/dotfiles/master/.bash_profile > ~/.bash_profile
+curl https://raw.githubusercontent.com/grant/dotfiles/master/.git-branch.sh > ~/.git-branch.sh
+curl https://raw.githubusercontent.com/grant/dotfiles/master/.git-prompt.sh > ~/.git-prompt.sh
