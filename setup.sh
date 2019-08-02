19691b07eaa15aa0174124feb2cf71f9871aa3d5
Grant Timmerman <timmerman+devrel@google.com>
2019-08-01 21:26:32 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,8 @@ curl https://raw.githubusercontent.com/grant/dotfiles/master/.bash_profile > ~/.
 curl https://raw.githubusercontent.com/grant/dotfiles/master/.git-branch.sh > ~/.git-branch.sh
 curl https://raw.githubusercontent.com/grant/dotfiles/master/.git-prompt.sh > ~/.git-prompt.sh
 
-sleep 1 # wait for download
+sleep 10 # wait for download
 
-cat ~/.bash_profile
-
-#source ~/.bash_profile
-#source ~/.git-branch.sh
-#source ~/.git-prompt.sh
+source ~/.bash_profile
+source ~/.git-branch.sh
+source ~/.git-prompt.sh
